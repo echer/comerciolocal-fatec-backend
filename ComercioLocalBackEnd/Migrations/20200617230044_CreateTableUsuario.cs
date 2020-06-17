a24b3ec9c494748b153101b6313b974f39d42476
alan echer <alan.echer@gmail.com>
2020-06-17 20:02:03 -0300

    migrations

---
+++ ComercioLocalBackEnd/Migrations/20200617230044_CreateTableUsuario.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComercioLocalBackEnd.Migrations
+{
+    public partial class CreateTableUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(nullable: false),
+                    DataNascimento = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    Senha = table.Column<string>(nullable: false),
+                    TelefoneMovel = table.Column<string>(nullable: false),
+                    TelefoneFixo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Perfil",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UsuarioId = table.Column<Guid>(nullable: false),
+                    Razao = table.Column<string>(nullable: false),
+                    Fantasia = table.Column<string>(nullable: false),
+                    DataAbertura = table.Column<string>(nullable: false),
+                    CNPJ = table.Column<string>(nullable: false),
+                    Site = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: false),
+                    CEP = table.Column<string>(nullable: false),
+                    Logradouro = table.Column<string>(nullable: false),
+                    Numero = table.Column<string>(nullable: true),
+                    Complemento = table.Column<string>(nullable: true),
+                    UF = table.Column<string>(nullable: false),
+                    Pais = table.Column<string>(nullable: false),
+                    Segmento = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Perfil", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Perfil_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Promocao",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    PerfilId = table.Column<Guid>(nullable: false),
+                    DataInicio = table.Column<string>(nullable: true),
+                    DataFim = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: false),
+                    MidiaPromocao = table.Column<string>(nullable: true),
+                    CupomPromocao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Promocao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Promocao_Perfil_PerfilId",
+                        column: x => x.PerfilId,
+                        principalTable: "Perfil",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Perfil_UsuarioId",
+                table: "Perfil",
+                column: "UsuarioId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Promocao_PerfilId",
+                table: "Promocao",
+                column: "PerfilId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Promocao");
+
+            migrationBuilder.DropTable(
+                name: "Perfil");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
---
+++ ComercioLocalBackEnd/Migrations/20200617230131_CreateTablePerfil.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComercioLocalBackEnd.Migrations
+{
+    public partial class CreateTablePerfil : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Perfil_Usuario_UsuarioId",
+                table: "Perfil");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Promocao_Perfil_PerfilId",
+                table: "Promocao");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuario",
+                table: "Usuario");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Promocao",
+                table: "Promocao");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Perfil",
+                table: "Perfil");
+
+            migrationBuilder.RenameTable(
+                name: "Usuario",
+                newName: "Usuarios");
+
+            migrationBuilder.RenameTable(
+                name: "Promocao",
+                newName: "Promocoes");
+
+            migrationBuilder.RenameTable(
+                name: "Perfil",
+                newName: "Perfis");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Promocao_PerfilId",
+                table: "Promocoes",
+                newName: "IX_Promocoes_PerfilId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Perfil_UsuarioId",
+                table: "Perfis",
+                newName: "IX_Perfis_UsuarioId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Promocoes",
+                table: "Promocoes",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Perfis",
+                table: "Perfis",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Perfis_Usuarios_UsuarioId",
+                table: "Perfis",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Promocoes_Perfis_PerfilId",
+                table: "Promocoes",
+                column: "PerfilId",
+                principalTable: "Perfis",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Perfis_Usuarios_UsuarioId",
+                table: "Perfis");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Promocoes_Perfis_PerfilId",
+                table: "Promocoes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Promocoes",
+                table: "Promocoes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Perfis",
+                table: "Perfis");
+
+            migrationBuilder.RenameTable(
+                name: "Usuarios",
+                newName: "Usuario");
+
+            migrationBuilder.RenameTable(
+                name: "Promocoes",
+                newName: "Promocao");
+
+            migrationBuilder.RenameTable(
+                name: "Perfis",
+                newName: "Perfil");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Promocoes_PerfilId",
+                table: "Promocao",
+                newName: "IX_Promocao_PerfilId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Perfis_UsuarioId",
+                table: "Perfil",
+                newName: "IX_Perfil_UsuarioId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuario",
+                table: "Usuario",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Promocao",
+                table: "Promocao",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Perfil",
+                table: "Perfil",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Perfil_Usuario_UsuarioId",
+                table: "Perfil",
+                column: "UsuarioId",
+                principalTable: "Usuario",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Promocao_Perfil_PerfilId",
+                table: "Promocao",
+                column: "PerfilId",
+                principalTable: "Perfil",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
